f29b88b1eb4609d5b8737d69b77e96e856711d15
unknown <priyesh.pandey321@gmail.com>
2021-04-04 18:16:29 +0530

    Error Page

--- Frontend/src/components/four.js
+++ Frontend/src/components/four.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Error from '../images/error.png'
+import {Link} from 'react-router-dom'
 const four=()=>{
     return(
         <>
-        <h1>four</h1>
+        <img src={Error} className="d-flex m-auto"/>
+        <Link to="/" className="d-flex justify-content-center text-danger text-bold">Go to Home Page</Link>
         </>
     )
 }
